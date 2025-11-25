c0cbfb90aaa50e3e64cc175d51cc1883bdb9babc
Ranjan <RanjanKumar.Choubey@walmart.com>
2025-11-25 15:24:42 +0530

    zmwm_data

---
+++ output/upload_dag.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcloud storage cp /Users/vn59a0h/Desktop/projects/ingestion_tool/output/configs/sa_supply_chain_dl_secure/mm_db_data_acrcy/INTLDLDAT-SAMM-INC-SA_SUPPLY_CHAIN_DL_SECURE-MM_DB_DATA_ACRCY.py gs://bfdaf-dags-intldlsadev-catalog/
